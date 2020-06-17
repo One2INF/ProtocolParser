540e205c928baea624a80176946573a794a1c9c2
One2INF <task_list_kill@163.com>
2020-06-17 10:37:24 +0800

    ui布局完成

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,25 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+#include <QStandardItemModel>
+
+
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+
+  QStandardItemModel* model = new QStandardItemModel(this);
+  model->setItem(0, 0, new QStandardItem(QString::fromLocal8Bit("张三")));
+  model->setItem(0, 1, new QStandardItem("3"));
+  model->setItem(0, 2, new QStandardItem(QString::fromLocal8Bit("名字")));
+  model->setItem(0, 3, new QStandardItem(QString::fromLocal8Bit("男")));
+
+  ui->tableView->setModel(model);
+  ui->tableView->setWindowTitle("pop");
+  ui->tableView->show();
 }
 
 MainWindow::~MainWindow()
